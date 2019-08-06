3a91f6f19248a5ffa9e4d86ae0acbbd78969927e
Chenchong Shi (史晨冲) <shichenchong@inspur.com>
2019-08-06 17:28:40 +0800

    创建数据库，创建登录信息表

---
+++ src/main/java/com/sceon/community/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.sceon.community.mapper;
+
+/**
+ * @author shichenchong@inspur.com
+ * data   2019/8/6 11:03
+ */
+public interface UserMapper {
+}
---
+++ src/main/java/com/sceon/community/model/User.java
@@ -0,0 +1,8 @@
+package com.sceon.community.model;
+
+/**
+ * @author shichenchong@inspur.com
+ * data   2019/8/6 11:05
+ */
+public class User {
+}
